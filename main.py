fe182bb51e85e851d428f5f328c37db5747d91fd
aleganza <ganzarolli.alessio@gmail.com>
2023-10-02 21:01:02 +0200

    add class names in json

--- main.py
+++ main.py
@@ -1,57 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
 class Scraper:
-    def __init__(this, url):
-        this.url = url
-        this.page = requests.get(url)
-        this.soup = BeautifulSoup(this.page.content, 'html.parser')
+    def __init__(self, url):
+        self.url = url
+        self.page = requests.get(url)
+        self.soup = BeautifulSoup(self.page.content, 'html.parser')
+        self.classNames = json.load(open('classNames.json'))
         
-    def getUrl(this):
-        return this.url
+    def getUrl(self):
+        return self.url
     
-    def getPage(this):
-        return this.page
+    def getPage(self):
+        return self.page
     
-    def getArticlesNumber(this):
-        return this.soup.find_all('p', {'class': 'index-module_sbt-text-atom__ed5J9 index-module_token-caption__TaQWv size-normal index-module_weight-semibold__MWtJJ total-ads AdsCount_total-ads__9aTXA'})[0].text
+    def getArticlesNumber(self):
+        return self.soup.find_all('p', {'class': self.classNames["articlesNumber"]})[0].text
     
-    def getPageTitle(this):
-        return this.soup.find_all('h1', {'class': 'index-module_sbt-text-atom__ed5J9 index-module_token-h6__FGmXw size-normal index-module_weight-semibold__MWtJJ'})[0].text
+    def getPageTitle(self):
+        return self.soup.find_all('h1', {'class': self.classNames["pageTitle"]})[0].text
 
-    def getPageArticles(this):
-        return this.soup.find_all('div', {'class': 'items__item item-card item-card--small'})
+    def getPageArticles(self):
+        return self.soup.find_all('div', {'class': self.classNames["pageArticles"]})
     
-    def getPageHref(this, article):
-        return article.find_all('a', href=True)[0]['href']
+    def getPageHref(self, article):
+        return article.find_all('a', href = True)[0]['href']
     
-    def getCardTitle(this, article):
-        return article.find('h2', {'class': 'index-module_sbt-text-atom__ed5J9 index-module_token-h6__FGmXw size-normal index-module_weight-semibold__MWtJJ ItemTitle-module_item-title__VuKDo SmallCard-module_item-title__1y5U3'}).text
+    def getCardTitle(self, article):
+        return article.find('h2', {'class': self.classNames["cardTitle"]}).text
 
-URL = 'https://www.subito.it/annunci-emilia-romagna/vendita/usato/ferrara/ferrara/?q=iPhone%2011'
-
-scraper = Scraper(URL)
-print(scraper.getUrl())
-
-if scraper.getPage().status_code:
-    articlesNumber = scraper.getArticlesNumber()
-    pageTitle = scraper.getPageTitle()
-    
-    print('Found ' + articlesNumber + ' for ' + pageTitle)
-    
-    articles = scraper.getPageArticles()
-    
-    for article in articles:
-        cardLink = scraper.getPageHref(article)
-        cardTitle = scraper.getCardTitle(article)
-        
-        # Check if the product is sold out or not
-        if article.find('p', {'class': 'index-module_price__N7M2x SmallCard-module_price__yERv7 index-module_small__4SyUf'}):
-            cardPrice = article.find('p', {'class': 'index-module_price__N7M2x SmallCard-module_price__yERv7 index-module_small__4SyUf'}).text
-        else:
-            cardPrice = article.find('p', {'class': 'index-module_price__N7M2x SmallCard-module_price__yERv7 index-module_no-item-available-price__v7iwv index-module_badge-below__CtGle index-module_small__4SyUf'}).text
-        
-        if "Venduto" not in cardPrice:
-            print(cardTitle + ': ' + cardPrice.replace('Spedizione disponibile', ''))
-            print(cardLink + '\n')
-          
