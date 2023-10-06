091687cbce26652fe3475f338b0819310319d6be
aleganza <ganzarolli.alessio@gmail.com>
2023-10-06 19:23:47 +0200

    remove npm stuff

--- src/test.py
+++ src/test.py
@@ -5,6 +5,8 @@ from scraper import Scraper
 urlBuilder = URLBuilder()
 scraper = Scraper()
 
+
+
 url = urlBuilder.buildUrl('iPhone 14 Pro', 'Bologna', 'usato', False, True, False)
 print(url)
 articles = scraper.getArticles(url)
